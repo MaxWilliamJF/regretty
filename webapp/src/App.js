8f2ed1a9b759edb6584fd6577bc4dec665e6a7b8
Max G. Damasceno <maxwilljf@gmail.com>
2018-04-15 17:21:52 -0300

    Basic structure

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
 
 class App extends Component {
+  checkValue() {
+    window.alert(Date.now());
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <Header />
+          <div className="crypto">
+            <h3>BTC</h3>
+            <h3>10k</h3>
+          </div>
+          <div className="card value">
+            <h4>U$$</h4>
+            <input placeholder="500" type="number" />
+          </div>
+          <div className="confirm">
+            <input type="button" value="DONE" onClick={this.checkValue} />            
+          </div>
       </div>
     );
   }
---
+++ webapp/src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Regretty</h1>
+                <h2>You (probably) should've done it before.</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
